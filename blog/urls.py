b26913ce6b761add7c660a01b305839250555f50
KristianYanakov <christianyanakov@gmail.com>
2025-05-21 11:01:11 +0300

    created a blogpost form to submit blogposts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.display_blogposts, name='display_blogposts'),
-
+    path('create/', views.create_blogpost, name='create_blogpost'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from .forms import BlogPostForm
 from .models import BlogPost
 # Create your views here.
 def display_blogposts(request):
@@ -8,3 +9,14 @@ def display_blogposts(request):
 
     return render(request, 'blog.html', {'blog_posts': blog_posts})
     # return HttpResponse("HELLO WORKING BLOGGGGGG")
+
+def create_blogpost(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST) # pass POST data to the form
+        if form.is_valid():
+            form.save() #save the blgopost to the databse
+            return redirect('display_blogposts') #redirect to the view with all of the blogposts
+    else:
+        form = BlogPostForm()  # Assign empty form for GET requests
+
+    return render(request, 'blog_post_form.html', {'form': form})
