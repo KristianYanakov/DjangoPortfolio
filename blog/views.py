6e74056962081aacd9b2fc4dca659be52e780605
KristianYanakov <christianyanakov@gmail.com>
2025-05-21 10:37:06 +0300

    Working Blog Posts Display and added a test blogpost via the shell

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import BlogPost
 # Create your views here.
+def display_blogposts(request):
+    blog_posts = BlogPost.objects.all()
+
+    return render(request, 'blog.html', {'blog_posts': blog_posts})
+    # return HttpResponse("HELLO WORKING BLOGGGGGG")
